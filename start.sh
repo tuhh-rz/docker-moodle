e9a1ab09e09e089535d5e7fa375d8b888ca3d0aa
Andreas Böttger <andreas.boettger@tuhh.de>
2016-05-06 18:06:38 +0200

    TODOs hinzugefügt

--- start.sh
+++ start.sh
@@ -5,7 +5,17 @@ SHARED_FOLDER="/usr/local/share/moodle"
 chown -Rf www-data.www-data /var/www/html/
 chown -Rf www-data.www-data "$SHARED_FOLDER"
 
+# Konfiguration in den Host plazieren
 ln -s "$SHARED_FOLDER"/config/config.php /var/www/html/moodle/config.php
+
+## TODO Plugin-Verzeichnisse im Host plazieren
+
+# > rsync um Aenderungen an den Themes zu übernehmen, die aus einem Release Upgrade stammen
+# rsync /var/www/html/moodle/theme $SHARED_FOLDER/themes
+# rm -rf /var/www/html/moodle/theme
+# ln -s "$SHARED_FOLDER"/themes /var/www/html/moodle/theme
+
+# moodledata in den Host plazieren
 ln -s "$SHARED_FOLDER"/moodledata /var/www
 
 chown -Rf www-data.www-data "$SHARED_FOLDER"/moodledata 
