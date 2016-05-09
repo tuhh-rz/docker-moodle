596f21f2701a53110b3fe6a160a5786f433d7f95
Andreas Böttger <andreas.boettger@tuhh.de>
2016-05-09 16:32:29 +0200

    Plugin Verzeichnis

--- start.sh
+++ start.sh
@@ -5,15 +5,22 @@ SHARED_FOLDER="/usr/local/share/moodle"
 chown -Rf www-data.www-data /var/www/html/
 chown -Rf www-data.www-data "$SHARED_FOLDER"
 
+if [ ! -d "$SHARED_FOLDER/config" ]; then
+    mkdir -p "$SHARED_FOLDER/config"
+fi
 # Konfiguration in den Host plazieren
 ln -s "$SHARED_FOLDER"/config/config.php /var/www/html/moodle/config.php
+
 
 ## TODO Plugin-Verzeichnisse im Host plazieren
 
 # > rsync um Aenderungen an den Themes zu übernehmen, die aus einem Release Upgrade stammen
-# rsync /var/www/html/moodle/theme $SHARED_FOLDER/themes
-# rm -rf /var/www/html/moodle/theme
-# ln -s "$SHARED_FOLDER"/themes /var/www/html/moodle/theme
+if [ ! -d "$SHARED_FOLDER/plugins" ]; then
+    mkdir -p "$SHARED_FOLDER/plugins"
+fi
+rsync -rc /var/www/html/moodle/theme "$SHARED_FOLDER/plugins"
+rm -rf /var/www/html/moodle/theme
+ln -s "$SHARED_FOLDER"/plugins/theme /var/www/html/moodle/theme
 
 # moodledata in den Host plazieren
 ln -s "$SHARED_FOLDER"/moodledata /var/www
