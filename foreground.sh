1ab3155181ea5db12f25cc052594cd452589d81a
Andreas Böttger <andreas.boettger@tuhh.de>
2016-08-05 15:11:42 +0200

    Logging

--- foreground.sh
+++ foreground.sh
@@ -4,4 +4,10 @@ read pid cmd state ppid pgrp session tty_nr tpgid rest < /proc/self/stat
 trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
 
 source /etc/apache2/envvars
+
+# logs should go to stdout / stderr
+set -ex \
+	&& ln -sfT /dev/stderr "$APACHE_LOG_DIR/error.log" \
+	&& ln -sfT /dev/stdout "$APACHE_LOG_DIR/access.log"
+	
 apache2 -D FOREGROUND
