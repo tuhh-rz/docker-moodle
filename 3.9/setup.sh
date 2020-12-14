ed5e1239a21402d4ff641a297a3a68ad3d8abf0f
Andreas Böttger <andreas.boettger@tuhh.de>
2020-12-14 16:50:36 +0100

    add RELPATH

--- 3.9/setup.sh
+++ 3.9/setup.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 export WEBSERVER_ROOT=${WEBSERVER_ROOT:-/var/www/html}
+export RELATIVE_PATH=${RELATIVE_PATH:-/}
 
-perl -i -pe 's/DocumentRoot.*/DocumentRoot $ENV{'WEBSERVER_ROOT'}\/moodle/g' /etc/apache2/sites-available/default-ssl.conf
-perl -i -pe 's/DocumentRoot.*/DocumentRoot $ENV{'WEBSERVER_ROOT'}\/moodle/g' /etc/apache2/sites-available/000-default.conf
+perl -i -pe 's/DocumentRoot.*/DocumentRoot $ENV{'WEBSERVER_ROOT'}/g' /etc/apache2/sites-available/default-ssl.conf
+perl -i -pe 's/DocumentRoot.*/DocumentRoot $ENV{'WEBSERVER_ROOT'}/g' /etc/apache2/sites-available/000-default.conf
 
 # Upload size
 sed -i 's/upload_max_filesize.*/upload_max_filesize = 1500M/g' /etc/php/7.4/apache2/php.ini
 sed -i 's/post_max_size.*/post_max_size = 1500M/g' /etc/php/7.4/apache2/php.ini
 sed -i 's/max_execution_time.*/max_execution_time = 600/g' /etc/php/7.4/apache2/php.ini
 
-echo "Sync Moodle into $WEBSERVER_ROOT"
-rsync -au /tmp/moodle "$WEBSERVER_ROOT"
+echo "$(date +"%Y-%m-%d %T %s") Sync Moodle into ${WEBSERVER_ROOT}${RELATIVE_PATH}"
+rsync -au /tmp/moodle/ "${WEBSERVER_ROOT}${RELATIVE_PATH}"
 
-echo "Fix permissions"
-find "$WEBSERVER_ROOT" ! -user www-data -exec chown www-data: {} +
+echo "$(date +"%Y-%m-%d %T %s") Fix permissions"
+find "${WEBSERVER_ROOT}" ! -user www-data -exec chown www-data: {} +
 
-echo "Install Moodle if neccesary"
+echo "$(date +"%Y-%m-%d %T %s") Install Moodle if neccesary"
 export CHMOD=${CHMOD:-2777}
 export LANG=${LANG:-en}
 export PREFIX=${PREFIX:-mdl_}
@@ -26,19 +27,19 @@ export DATAROOT=${DATAROOT:-/usr/local/share/moodle/moodledata}
 mkdir -p "$DATAROOT"
 chmod 0777 "$DATAROOT"
 
-su -s /bin/bash -c "/usr/bin/php $WEBSERVER_ROOT/moodle/admin/cli/install.php --non-interactive --agree-license --dataroot=$DATAROOT --chmod=$CHMOD --lang=$LANG --wwwroot=$WWWROOT --dbtype=$DBTYPE --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbport=$DBPORT --prefix=$PREFIX --fullname='$FULLNAME' --shortname='$SHORTNAME' --summary='$SUMMARY' --adminuser=$ADMINUSER --adminpass=$ADMINPASS --adminemail=$ADMINEMAIL" www-data
+su -s /bin/bash -c "/usr/bin/php ${WEBSERVER_ROOT}${RELATIVE_PATH}admin/cli/install.php --non-interactive --agree-license --dataroot=$DATAROOT --chmod=$CHMOD --lang=$LANG --wwwroot=$WWWROOT --dbtype=$DBTYPE --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbport=$DBPORT --prefix=$PREFIX --fullname='$FULLNAME' --shortname='$SHORTNAME' --summary='$SUMMARY' --adminuser=$ADMINUSER --adminpass=$ADMINPASS --adminemail=$ADMINEMAIL" www-data
 
-echo "Upgrade Moodle if neccesary"
-su -s /bin/bash -c "/usr/bin/php $WEBSERVER_ROOT/moodle/admin/cli/upgrade.php --non-interactive" www-data
+echo "$(date +"%Y-%m-%d %T %s") Upgrade Moodle if neccesary"
+su -s /bin/bash -c "/usr/bin/php ${WEBSERVER_ROOT}${RELATIVE_PATH}admin/cli/upgrade.php --non-interactive" www-data
 
-echo "Ensure cron.php"
-echo "*/5 * * * * www-data /usr/bin/php $WEBSERVER_ROOT/moodle/admin/cli/cron.php" >>/etc/crontab
+echo "$(date +"%Y-%m-%d %T %s") Ensure cron.php"
+echo "*/5 * * * * www-data /usr/bin/php ${WEBSERVER_ROOT}${RELATIVE_PATH}admin/cli/cron.php" >>/etc/crontab
 
 # It is recommended that the file permissions of config.php are changed after
 # installation so that the file cannot be modified by the web server. Please
 # note that this measure does not improve security of the server significantly,
 # though it may slow down or limit general exploits.
-chmod -w "$WEBSERVER_ROOT"/moodle/config.php
+chmod -w "${WEBSERVER_ROOT}${RELATIVE_PATH}"config.php
 
 ln -sf /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled/
 
