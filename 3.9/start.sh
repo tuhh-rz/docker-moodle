40635215455fd80e45003213b85d707bc4df7406
Andreas Böttger <andreas.boettger@tuhh.de>
2020-12-14 12:23:30 +0100

    fix something

--- 3.9/start.sh
+++ 3.9/start.sh
@@ -13,8 +13,8 @@ sed -i '/SSLCertificateChainFile/d' /etc/apache2/sites-available/default-ssl.con
 
 sed -i 's/SSLEngine.*/SSLEngine on\nSSLCertificateFile \/etc\/apache2\/ssl\/cert.pem\nSSLCertificateKeyFile \/etc\/apache2\/ssl\/private_key.pem\nSSLCertificateChainFile \/etc\/apache2\/ssl\/cert-chain.pem/' /etc/apache2/sites-available/default-ssl.conf
 
-sed -i 's/DocumentRoot.*/DocumentRoot \/var\/www\/html\/moodle/' /etc/apache2/sites-available/default-ssl.conf
-sed -i 's/DocumentRoot.*/DocumentRoot \/var\/www\/html\/moodle/' /etc/apache2/sites-available/000-default.conf
+perl -i -pe 's/DocumentRoot.*/DocumentRoot $ENV{'WEBSERVER_ROOT'}/g' /etc/apache2/sites-available/default-ssl.conf
+perl -i -pe 's/DocumentRoot.*/DocumentRoot $ENV{'WEBSERVER_ROOT'}/g' /etc/apache2/sites-available/000-default.conf
 
 # Upload size
 sed -i 's/upload_max_filesize.*/upload_max_filesize = 1500M/g' /etc/php/7.4/apache2/php.ini
@@ -30,35 +30,17 @@ export LANG=${LANG:-en}
 export PREFIX=${PREFIX:-mdl_}
 export DBPORT=${DBPORT:-3306}
 
-su -s /bin/bash -c "/usr/bin/php $WEBSERVER_ROOT/admin/cli/install.php \
-  --non-interactive \
-  --agree-license \
-  --chmod=$CHMOD \
-  --lang=$LANG \
-  --wwwroot=$WWWROOT \
-  --dbtype=$DBTYPE \
-  --dbhost=$DBHOST \
-  --dbname=$DBNAME \
-  --dbuser=$DBUSER \
-  --dbpass=$DBPASS \
-  --dbport=$DBPORT \
-  --prefix=$PREFIX \
-  --fullname='$FULLNAME' \
-  --shortname='$SHORTNAME' \
-  --summary='$SUMMARY' \
-  --adminuser=$ADMINUSER \
-  --adminpass=$ADMINPASS \
-  --adminemail=$ADMINEMAIL \
-" www-data
+su -s /bin/bash -c "/usr/bin/php $WEBSERVER_ROOT/admin/cli/install.php --non-interactive --agree-license --chmod=$CHMOD --lang=$LANG --wwwroot=$WWWROOT --dbtype=$DBTYPE --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbport=$DBPORT --prefix=$PREFIX --fullname='$FULLNAME' --shortname='$SHORTNAME' --summary='$SUMMARY' --adminuser=$ADMINUSER --adminpass=$ADMINPASS --adminemail=$ADMINEMAIL" www-data
 
 echo "Upgrade Moodle if neccesary"
-su -s /bin/bash -c "/usr/bin/php $WEBSERVER_ROOT/admin/cli/upgrade.php \
-  --non-interactive \
-" www-data
+su -s /bin/bash -c "/usr/bin/php $WEBSERVER_ROOT/admin/cli/upgrade.php --non-interactive" www-data
 
 echo "Fix permissions"
 find "$WEBSERVER_ROOT" ! -user www-data -exec chown www-data: {} +
 find /var/www/moodledata ! -user www-data -exec chown www-data: {} +
+
+echo "Ensure cron.php"
+echo "*/5 * * * * www-data /usr/bin/php $WEBSERVER_ROOT/moodle/admin/cli/cron.php" >>/etc/crontab
 
 # It is recommended that the file permissions of config.php are changed after
 # installation so that the file cannot be modified by the web server. Please
