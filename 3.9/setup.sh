6291f582f4623ef1bd011156945ebe1c8c371451
Andreas Böttger <andreas.boettger@tuhh.de>
2020-12-14 15:21:11 +0100

    make it earlier

--- 3.9/setup.sh
+++ 3.9/setup.sh
@@ -13,6 +13,9 @@ sed -i 's/max_execution_time.*/max_execution_time = 600/g' /etc/php/7.4/apache2/
 echo "Sync Moodle into $WEBSERVER_ROOT"
 rsync -au /tmp/moodle "$WEBSERVER_ROOT"
 
+echo "Fix permissions"
+find "$WEBSERVER_ROOT" ! -user www-data -exec chown www-data: {} +
+
 echo "Install Moodle if neccesary"
 export CHMOD=${CHMOD:-2777}
 export LANG=${LANG:-en}
@@ -22,9 +25,6 @@ export DBPORT=${DBPORT:-3306}
 export DATAROOT=${DATAROOT:-/usr/local/share/moodle/moodledata}
 mkdir -p "$DATAROOT"
 chmod 0777 "$DATAROOT"
-
-echo "Fix permissions"
-find "$WEBSERVER_ROOT" ! -user www-data -exec chown www-data: {} +
 
 su -s /bin/bash -c "/usr/bin/php $WEBSERVER_ROOT/moodle/admin/cli/install.php --non-interactive --agree-license --dataroot=$DATAROOT --chmod=$CHMOD --lang=$LANG --wwwroot=$WWWROOT --dbtype=$DBTYPE --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbport=$DBPORT --prefix=$PREFIX --fullname='$FULLNAME' --shortname='$SHORTNAME' --summary='$SUMMARY' --adminuser=$ADMINUSER --adminpass=$ADMINPASS --adminemail=$ADMINEMAIL" www-data
 
