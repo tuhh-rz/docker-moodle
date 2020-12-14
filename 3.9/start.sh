4126cec1026d71de4da31121bb2d35a42d75b468
Andreas Böttger <andreas.boettger@tuhh.de>
2020-12-14 11:16:02 +0100

    use install.php

--- 3.9/start.sh
+++ 3.9/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 SHARED_FOLDER="/usr/local/share/moodle/moodledata"
+WEBSERVER_ROOT="/var/www/html"
+
 ln -s "$SHARED_FOLDER" /var/www
 
 /usr/sbin/a2enmod ssl
@@ -19,17 +21,46 @@ sed -i 's/upload_max_filesize.*/upload_max_filesize = 1500M/g' /etc/php/7.4/apac
 sed -i 's/post_max_size.*/post_max_size = 1500M/g' /etc/php/7.4/apache2/php.ini
 sed -i 's/max_execution_time.*/max_execution_time = 600/g' /etc/php/7.4/apache2/php.ini
 
-rsync -au /tmp/moodle/ /var/www/html
+rsync -au /tmp/moodle/ "$WEBSERVER_ROOT"
 
-cd /var/www/html && /usr/bin/php admin/cli/upgrade.php --non-interactive
-find /var/www/html ! -user www-data -exec chown www-data: {} +
+export CHMOD=${CHMOD:-2777}
+export LANG=${LANG:-en}
+export PREFIX=${PREFIX:-mdl_}
+export DBPORT=${DBPORT:-3306}
+
+su -s /bin/bash -c "/usr/bin/php $WEBSERVER_ROOT/admin/cli/install.php
+  --non-interactive
+  --agree-license
+  --chmod=$CHMOD
+  --lang=$LANG
+  --WWWROOT=$WWWROOT
+  --dbtype=$DBTYPE
+  --dbhost=$DBHOST
+  --dbname=$DBNAME
+  --dbuser=$DBUSER
+  --dbpass=$DBPASS
+  --dbport=$DBPORT
+  --prefix=$PREFIX
+  --fullname=$FULLNAME
+  --shortname=$SHORTNAME
+  --summary=$SUMMARY
+  --adminuser=$ADMINUSER
+  --adminpass=$ADMINPASS
+  --adminemail=$ADMINEMAIL
+" www-data
+
+su -s /bin/bash -c "/usr/bin/php $WEBSERVER_ROOT/admin/cli/upgrade.php
+  --non-interactive
+" www-data
+
+find "$WEBSERVER_ROOT" ! -user www-data -exec chown www-data: {} +
 find /var/www/moodledata ! -user www-data -exec chown www-data: {} +
 
 # It is recommended that the file permissions of config.php are changed after
 # installation so that the file cannot be modified by the web server. Please
 # note that this measure does not improve security of the server significantly,
 # though it may slow down or limit general exploits.
-chmod -w /var/www/html/config.php
+chmod -w "$WEBSERVER_ROOT"/config.php
 
 ln -s /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled/
 
